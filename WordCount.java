7dc0a786af85bd097916d0301a500e5ba8e60d59
m24aid048 <m24aid048@iitj.ac.in>
2026-02-12 20:28:23 +0530

    Add files via upload

---
+++ WordCount.java
@@ -0,0 +1,105 @@
+import java.io.IOException;
+import java.util.StringTokenizer;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.Reducer;
+
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+
+public class WordCount {
+
+    /* ===================== MAPPER ===================== */
+
+
+
+
+public static class Map
+        extends Mapper<LongWritable, Text, Text, IntWritable> {
+
+    private final static IntWritable one = new IntWritable(1);
+    private Text word = new Text();
+
+    @Override
+    public void map(LongWritable key, Text value, Context context)
+            throws IOException, InterruptedException {
+
+        String line = value.toString()
+                           .toLowerCase()
+                           .replaceAll("[^a-z ]", "");
+
+        StringTokenizer tokenizer = new StringTokenizer(line);
+
+        while (tokenizer.hasMoreTokens()) {
+            word.set(tokenizer.nextToken());
+            context.write(word, one);
+        }
+    }
+}
+
+
+
+    /* ===================== REDUCER ===================== */
+
+    public static class Reduce
+            extends Reducer<Text, IntWritable, Text, IntWritable> {
+
+        private IntWritable result = new IntWritable();
+
+        @Override
+        public void reduce(Text key, Iterable<IntWritable> values, Context context)
+                throws IOException, InterruptedException {
+
+            int sum = 0;
+            for (IntWritable val : values) {
+                sum += val.get();
+            }
+
+            result.set(sum);
+            context.write(key, result);
+        }
+    }
+
+    /* ===================== DRIVER ===================== */
+
+    public static void main(String[] args) throws Exception {
+
+        Configuration conf = new Configuration();
+
+conf.setLong("mapreduce.input.fileinputformat.split.maxsize", 134217728);
+
+long startTime = System.currentTimeMillis();
+
+        Job job = Job.getInstance(conf, "word count");
+
+        job.setJarByClass(WordCount.class);
+
+        job.setMapperClass(Map.class);
+        job.setReducerClass(Reduce.class);
+
+        job.setOutputKeyClass(Text.class);
+        job.setOutputValueClass(IntWritable.class);
+
+        FileInputFormat.addInputPath(job, new Path(args[0]));
+        FileOutputFormat.setOutputPath(job, new Path(args[1]));
+
+        //System.exit(job.waitForCompletion(true) ? 0 : 1);
+
+	boolean success = job.waitForCompletion(true);
+
+	long endTime = System.currentTimeMillis();
+	System.out.println("Total execution time (ms): " + (endTime - startTime));
+
+	System.exit(success ? 0 : 1);
+
+
+    }
+}
